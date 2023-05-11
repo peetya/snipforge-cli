a68de9a247c08c5a381d6715827c4bc76c1ebaf0
Péter Halász <18699247+peetya@users.noreply.github.com>
2023-05-11 21:04:47 +0200

    chore: removed comments

--- util/fs.go
+++ util/fs.go
@@ -5,17 +5,6 @@ import (
 	"path/filepath"
 )
 
-func PrepareOutputFolderPath(output string) error {
-	//dirPath := filepath.Dir(output)
-	//if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-	//	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
-	//		return err
-	//	}
-	//}
-
-	return nil
-}
-
 func SaveSnippet(snippet string, output string) error {
 	dirPath := filepath.Dir(output)
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
